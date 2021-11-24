85bb75c62bee2f7c3561379b2f7e31b4579c95f6
Clément Dreptin <cdreptin@gmail.com>
2021-11-24 17:09:38 +0100

    refactor(test): reorganized the tests descriptions

--- test/run.js
+++ test/run.js
@@ -5,144 +5,142 @@ const { expect } = require('chai');
 const rewire = require('rewire');
 const { select } = require('../index');
 
-describe('index.js', () => {
-  describe('#select()', () => {
-    const testData = require('./dataset/in/select');
-    const expected = require('./dataset/expected/select');
+describe('select.js', () => {
+  const testData = require('./dataset/in/select');
+  const expected = require('./dataset/expected/select');
 
-    it('Fail: no docObjects were found', () => {
-      expectError(testData.noDocObjects, 'docObjects not found');
-    });
-
-    it('Fail: no docObjects with a source were found', () => {
-      expectError(testData.noDocObjectsWithSource, 'docObjects with source not found');
-    });
-
-    it('Fail: no docObjects with a known source were found', () => {
-      expectError(testData.noDocObjectsWithKnownSource, 'docObject corresponding to one of given sources not found');
-    });
-
-    it('Fail: no priorities were found', () => {
-      expectError(testData.noPriorities, 'priorities not found');
-    });
-
-    it('Success: global priorities)', () => {
-      expectSuccess(testData.globalPriorities, expected.globalPriorities);
-    });
-
-    it('Success: global and custom priorities', () => {
-      expectSuccess(testData.globalAndCustomPriorities, expected.globalAndCustomPriorities);
-    });
-
-    it('Success: global and custom priorities but no data', () => {
-      expectSuccess(testData.globalAndCustomPrioritiesButNoData, expected.globalAndCustomPrioritiesButNoData);
-    });
-
-    it('Success: global priorities for 2 identical sources and and delete unwanted data', () => {
-      expectSuccess(testData.globalPrioritiesAndDeleteUnwantedData, expected.globalPrioritiesAndDeleteUnwantedData);
-    });
-
-    it('Success: default prorities and Hal without a fulltext', () => {
-      expectSuccess(testData.defaultPrioritiesAndHalWithoutFulltext, expected.defaultPrioritiesAndHalWithoutFulltext);
-    });
-
-    it('Success: default prorities and Hal with a fulltext', () => {
-      expectSuccess(testData.defaultPrioritiesAndHalWithFulltext, expected.defaultPrioritiesAndHalWithFulltext);
-    });
-
-    it('Success: merge duplicates with idConditor', () => {
-      expectSuccess(testData.mergeDuplicates, expected.mergeDuplicates);
-    });
-
-    it('Success: merge duplicates with idConditor (one empty duplicate)', () => {
-      expectSuccess(testData.mergeDuplicatesWithOneEmptyDuplicate, expected.mergeDuplicatesWithOneEmptyDuplicate);
-    });
-
-    it('Success: merge duplicates with idConditor (identical duplicates)', () => {
-      expectSuccess(testData.mergeDuplicatesWithSameDuplicates, expected.mergeDuplicatesWithSameDuplicates);
-    });
-
-    it('Success: merge sourceUid', () => {
-      expectSuccess(testData.mergeSourceUid, expected.mergeSourceUid);
-    });
-
-    it('Success: merge sourceUid (same sourceUid)', () => {
-      expectSuccess(testData.mergeSourceUidButSameSourceUid, expected.mergeSourceUidButSameSourceUid);
-    });
-
-    it('Success: merge enrichments', () => {
-      expectSuccess(testData.mergeEnrichments, expected.mergeEnrichments);
-    });
-
-    it('Success: merge keywords', () => {
-      expectSuccess(testData.mergeKeywords, expected.mergeKeywords);
-    });
-
-    it('Success: merge orcId', () => {
-      expectSuccess(testData.mergeOrcId, expected.mergeOrcId);
-    });
+  it('Fail: no docObjects were found', () => {
+    expectError(testData.noDocObjects, 'docObjects not found');
   });
 
-  describe('#generateHalTEI()', () => {
-    const generateHalTEIModule = rewire('../src/generateHalTEI.js');
-    const testData = require('./dataset/in/generateHalTEI');
-    const biblFull = {};
+  it('Fail: no docObjects with a source were found', () => {
+    expectError(testData.noDocObjectsWithSource, 'docObjects with source not found');
+  });
 
-    it('Success: identifiers', () => {
-      const insertIdentifiers = generateHalTEIModule.__get__('insertIdentifiers');
-      insertIdentifiers(biblFull, testData.correctRecord);
+  it('Fail: no docObjects with a known source were found', () => {
+    expectError(testData.noDocObjectsWithKnownSource, 'docObject corresponding to one of given sources not found');
+  });
 
-      expect(biblFull.sourceDesc.biblStruct.idno).to.deep.include({ '@type': 'doi', '#': '10.1039/c8nr07898j' });
-      expect(biblFull.sourceDesc.biblStruct.monogr.idno).to.deep.include({ '@type': 'issn', '#': ['2040-3364'] });
-      expect(biblFull.sourceDesc.biblStruct.monogr.idno).to.deep.include({ '@type': 'eissn', '#': ['2040-3372'] });
-    });
+  it('Fail: no priorities were found', () => {
+    expectError(testData.noPriorities, 'priorities not found');
+  });
 
-    it('Success: abstract', () => {
-      const insertAbstract = generateHalTEIModule.__get__('insertAbstract');
-      insertAbstract(biblFull, testData.correctRecord);
+  it('Success: global priorities)', () => {
+    expectSuccess(testData.globalPriorities, expected.globalPriorities);
+  });
 
-      expect(biblFull.profileDesc.abstract.p.length).to.be.greaterThan(0);
-    });
+  it('Success: global and custom priorities', () => {
+    expectSuccess(testData.globalAndCustomPriorities, expected.globalAndCustomPriorities);
+  });
 
-    it('Success: language', () => {
-      const insertLanguage = generateHalTEIModule.__get__('insertLanguage');
-      insertLanguage(biblFull, testData.correctRecord);
+  it('Success: global and custom priorities but no data', () => {
+    expectSuccess(testData.globalAndCustomPrioritiesButNoData, expected.globalAndCustomPrioritiesButNoData);
+  });
 
-      expect(biblFull.profileDesc.langUsage.language[0]['@ident']).to.be.equal('en');
-      expect(biblFull.profileDesc.langUsage.language[0]['#']).to.be.equal('English');
-    });
+  it('Success: global priorities for 2 identical sources and and delete unwanted data', () => {
+    expectSuccess(testData.globalPrioritiesAndDeleteUnwantedData, expected.globalPrioritiesAndDeleteUnwantedData);
+  });
 
-    it('Success: titles', () => {
-      const insertTitles = generateHalTEIModule.__get__('insertTitles');
-      insertTitles(biblFull, testData.correctRecord);
+  it('Success: default prorities and Hal without a fulltext', () => {
+    expectSuccess(testData.defaultPrioritiesAndHalWithoutFulltext, expected.defaultPrioritiesAndHalWithoutFulltext);
+  });
 
-      expect(biblFull.titleStmt.title[0]['@xml:lang']).to.be.equal('en');
-      expect(biblFull.titleStmt.title[0]['#']).to.be.equal('Unexpected redox behaviour of large surface alumina containing highly dispersed ceria nanoclusters.');
-      expect(biblFull.sourceDesc.biblStruct.analytic.title).to.eql(biblFull.titleStmt.title);
-    });
+  it('Success: default prorities and Hal with a fulltext', () => {
+    expectSuccess(testData.defaultPrioritiesAndHalWithFulltext, expected.defaultPrioritiesAndHalWithFulltext);
+  });
 
-    it('Success: authors', () => {
-      const insertAuthors = generateHalTEIModule.__get__('insertAuthors');
-      insertAuthors(biblFull, testData.correctRecord);
+  it('Success: merge duplicates with idConditor', () => {
+    expectSuccess(testData.mergeDuplicates, expected.mergeDuplicates);
+  });
 
-      expect(biblFull.titleStmt.author.length).to.be.equal(7);
-      expect(biblFull.titleStmt.author[0]['@role']).to.be.equal('aut');
-      expect(biblFull.titleStmt.author[0].persName.forename['@type']).to.be.equal('first');
-      expect(biblFull.titleStmt.author[0].persName.forename['#']).to.be.equal('Juliana');
-      expect(biblFull.titleStmt.author[0].persName.surname).to.be.equal('Fonseca');
-      expect(biblFull.sourceDesc.biblStruct.analytic.author).to.eql(biblFull.titleStmt.author);
-    });
+  it('Success: merge duplicates with idConditor (one empty duplicate)', () => {
+    expectSuccess(testData.mergeDuplicatesWithOneEmptyDuplicate, expected.mergeDuplicatesWithOneEmptyDuplicate);
+  });
 
-    it('Success: catalog data', () => {
-      const insertCatalogData = generateHalTEIModule.__get__('insertCatalogData');
-      insertCatalogData(biblFull, testData.correctRecord);
+  it('Success: merge duplicates with idConditor (identical duplicates)', () => {
+    expectSuccess(testData.mergeDuplicatesWithSameDuplicates, expected.mergeDuplicatesWithSameDuplicates);
+  });
 
-      expect(biblFull.sourceDesc.biblStruct.monogr.imprint.biblScope).to.deep.include({ '@unit': 'issue', '#': '3' });
-      expect(biblFull.sourceDesc.biblStruct.monogr.imprint.biblScope).to.deep.include({ '@unit': 'pp', '#': '1273-1285' });
-      expect(biblFull.sourceDesc.biblStruct.monogr.imprint.biblScope).to.deep.include({ '@unit': 'volume', '#': '11' });
-      expect(biblFull.sourceDesc.biblStruct.monogr.imprint.date).to.deep.include({ '@type': 'datePub', '#': '2019-01-17' });
-      expect(biblFull.sourceDesc.biblStruct.monogr.imprint.date).to.deep.include({ '@type': 'dateEpub', '#': '2019' });
-    });
+  it('Success: merge sourceUid', () => {
+    expectSuccess(testData.mergeSourceUid, expected.mergeSourceUid);
+  });
+
+  it('Success: merge sourceUid (same sourceUid)', () => {
+    expectSuccess(testData.mergeSourceUidButSameSourceUid, expected.mergeSourceUidButSameSourceUid);
+  });
+
+  it('Success: merge enrichments', () => {
+    expectSuccess(testData.mergeEnrichments, expected.mergeEnrichments);
+  });
+
+  it('Success: merge keywords', () => {
+    expectSuccess(testData.mergeKeywords, expected.mergeKeywords);
+  });
+
+  it('Success: merge orcId', () => {
+    expectSuccess(testData.mergeOrcId, expected.mergeOrcId);
+  });
+});
+
+describe('generateHalTEI.js', () => {
+  const generateHalTEIModule = rewire('../src/generateHalTEI.js');
+  const testData = require('./dataset/in/generateHalTEI');
+  const biblFull = {};
+
+  it('Success: identifiers', () => {
+    const insertIdentifiers = generateHalTEIModule.__get__('insertIdentifiers');
+    insertIdentifiers(biblFull, testData.correctRecord);
+
+    expect(biblFull.sourceDesc.biblStruct.idno).to.deep.include({ '@type': 'doi', '#': '10.1039/c8nr07898j' });
+    expect(biblFull.sourceDesc.biblStruct.monogr.idno).to.deep.include({ '@type': 'issn', '#': ['2040-3364'] });
+    expect(biblFull.sourceDesc.biblStruct.monogr.idno).to.deep.include({ '@type': 'eissn', '#': ['2040-3372'] });
+  });
+
+  it('Success: abstract', () => {
+    const insertAbstract = generateHalTEIModule.__get__('insertAbstract');
+    insertAbstract(biblFull, testData.correctRecord);
+
+    expect(biblFull.profileDesc.abstract.p.length).to.be.greaterThan(0);
+  });
+
+  it('Success: language', () => {
+    const insertLanguage = generateHalTEIModule.__get__('insertLanguage');
+    insertLanguage(biblFull, testData.correctRecord);
+
+    expect(biblFull.profileDesc.langUsage.language[0]['@ident']).to.be.equal('en');
+    expect(biblFull.profileDesc.langUsage.language[0]['#']).to.be.equal('English');
+  });
+
+  it('Success: titles', () => {
+    const insertTitles = generateHalTEIModule.__get__('insertTitles');
+    insertTitles(biblFull, testData.correctRecord);
+
+    expect(biblFull.titleStmt.title[0]['@xml:lang']).to.be.equal('en');
+    expect(biblFull.titleStmt.title[0]['#']).to.be.equal('Unexpected redox behaviour of large surface alumina containing highly dispersed ceria nanoclusters.');
+    expect(biblFull.sourceDesc.biblStruct.analytic.title).to.eql(biblFull.titleStmt.title);
+  });
+
+  it('Success: authors', () => {
+    const insertAuthors = generateHalTEIModule.__get__('insertAuthors');
+    insertAuthors(biblFull, testData.correctRecord);
+
+    expect(biblFull.titleStmt.author.length).to.be.equal(7);
+    expect(biblFull.titleStmt.author[0]['@role']).to.be.equal('aut');
+    expect(biblFull.titleStmt.author[0].persName.forename['@type']).to.be.equal('first');
+    expect(biblFull.titleStmt.author[0].persName.forename['#']).to.be.equal('Juliana');
+    expect(biblFull.titleStmt.author[0].persName.surname).to.be.equal('Fonseca');
+    expect(biblFull.sourceDesc.biblStruct.analytic.author).to.eql(biblFull.titleStmt.author);
+  });
+
+  it('Success: catalog data', () => {
+    const insertCatalogData = generateHalTEIModule.__get__('insertCatalogData');
+    insertCatalogData(biblFull, testData.correctRecord);
+
+    expect(biblFull.sourceDesc.biblStruct.monogr.imprint.biblScope).to.deep.include({ '@unit': 'issue', '#': '3' });
+    expect(biblFull.sourceDesc.biblStruct.monogr.imprint.biblScope).to.deep.include({ '@unit': 'pp', '#': '1273-1285' });
+    expect(biblFull.sourceDesc.biblStruct.monogr.imprint.biblScope).to.deep.include({ '@unit': 'volume', '#': '11' });
+    expect(biblFull.sourceDesc.biblStruct.monogr.imprint.date).to.deep.include({ '@type': 'datePub', '#': '2019-01-17' });
+    expect(biblFull.sourceDesc.biblStruct.monogr.imprint.date).to.deep.include({ '@type': 'dateEpub', '#': '2019' });
   });
 });
 
